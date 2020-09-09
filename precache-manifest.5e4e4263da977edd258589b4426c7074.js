f6293e02baf86c5c4091462255c9ab63eaf3e8e5
Daniel Beckley <tomiwabeckley@gmail.com>
2020-09-09 12:44:30 +0100

    Updates

---
+++ precache-manifest.5e4e4263da977edd258589b4426c7074.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6feda03971395fcdaa7ee964d28477ce",
+    "url": "/welkin/index.html"
+  },
+  {
+    "revision": "4eab34187c8d45fcf6ac",
+    "url": "/welkin/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "75049c80ece305676295",
+    "url": "/welkin/static/css/main.09a54092.chunk.css"
+  },
+  {
+    "revision": "4eab34187c8d45fcf6ac",
+    "url": "/welkin/static/js/2.43c879aa.chunk.js"
+  },
+  {
+    "revision": "ef44b62912e2f9401d328f8015e6997d",
+    "url": "/welkin/static/js/2.43c879aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75049c80ece305676295",
+    "url": "/welkin/static/js/main.7f1ebfa1.chunk.js"
+  },
+  {
+    "revision": "d906195aa256fb90e680",
+    "url": "/welkin/static/js/runtime-main.aa5d74b2.js"
+  },
+  {
+    "revision": "e31b819ee1c22f8c62f824d33f9d56e1",
+    "url": "/welkin/static/media/DiscussionClass.e31b819e.svg"
+  },
+  {
+    "revision": "b8e8659ede2677f36939d5d007e81a4f",
+    "url": "/welkin/static/media/Library.b8e8659e.svg"
+  },
+  {
+    "revision": "165bc308ea4776422d9b3a64d4957163",
+    "url": "/welkin/static/media/StudentQuestion.165bc308.svg"
+  },
+  {
+    "revision": "009e73808ba61c0ed197f95aebcf6bb6",
+    "url": "/welkin/static/media/Trophy.009e7380.svg"
+  },
+  {
+    "revision": "e9d6671d88be122165013e9873ed8bc9",
+    "url": "/welkin/static/media/award.e9d6671d.jpg"
+  },
+  {
+    "revision": "6b2da4a4c9f23b21b0790252cbe42edc",
+    "url": "/welkin/static/media/call.6b2da4a4.svg"
+  },
+  {
+    "revision": "5f50531a279e8672a19995f5dc6339fe",
+    "url": "/welkin/static/media/decleft.5f50531a.jpg"
+  },
+  {
+    "revision": "01949be5541901ff5b05a4f41d7801df",
+    "url": "/welkin/static/media/decright.01949be5.jpg"
+  },
+  {
+    "revision": "817c898d032a76071064b4218bee3928",
+    "url": "/welkin/static/media/facebook.817c898d.svg"
+  },
+  {
+    "revision": "f4ecdf6969e6c31aca2aa1aee2818a90",
+    "url": "/welkin/static/media/feedback.f4ecdf69.jpg"
+  },
+  {
+    "revision": "c09c6adcfa43ed4736ffdcf26494371b",
+    "url": "/welkin/static/media/founder.c09c6adc.jpg"
+  },
+  {
+    "revision": "04c3a9b0ee1956f06feb67a4f16b6648",
+    "url": "/welkin/static/media/google-plus.04c3a9b0.svg"
+  },
+  {
+    "revision": "bcef6bbeee8cff1deb52f208dbc02882",
+    "url": "/welkin/static/media/instagram.bcef6bbe.svg"
+  },
+  {
+    "revision": "1ca9d2eab2e7a2240dd71d8fbc6de700",
+    "url": "/welkin/static/media/location.1ca9d2ea.svg"
+  },
+  {
+    "revision": "f6537db63b9a519ad7a7cb91b084828b",
+    "url": "/welkin/static/media/mail.f6537db6.svg"
+  },
+  {
+    "revision": "1e4419f16e89471282ecb74868a875c7",
+    "url": "/welkin/static/media/twitter.1e4419f1.svg"
+  },
+  {
+    "revision": "964b053b443414a95fb05313b5615fd9",
+    "url": "/welkin/static/media/undraw_contact_us_15o2.964b053b.svg"
+  },
+  {
+    "revision": "6293a5d8c89e5e8b014efadd1aa65ec7",
+    "url": "/welkin/static/media/undraw_under_construction_46pa.6293a5d8.svg"
+  },
+  {
+    "revision": "f2f6753578dd3d2e0b6fdb4c94388830",
+    "url": "/welkin/static/media/wclogofull.f2f67535.png"
+  },
+  {
+    "revision": "788a43cfeca06929d8897b274af4a3eb",
+    "url": "/welkin/static/media/youtube.788a43cf.svg"
+  }
+]);
