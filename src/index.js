163a003f34aa0666ecc04e84a6432c27731e32ca
Daniel Beckley <tomiwabeckley@gmail.com>
2020-11-21 22:38:15 +0100

    hashrouter and homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -36,9 +36,9 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
   </ApolloProvider>,
   document.getElementById('root')
 );
